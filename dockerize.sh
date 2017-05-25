477a691a1123731c376b0ba9940bad1bbc3ce49d
bjohnson <ben@gophronesis.com>
2017-05-25 11:05:49 -0400

    updating gpu-docker

--- dockerize.sh
+++ dockerize.sh
@@ -12,8 +12,8 @@ if [ $# -eq 0 ]
 fi
 
 config=$1
-imname=$(cat $config | jq -r .image_name)
-config_version=$(cat $config | jq -r ".dw_version //empty")
+imname=$(jq -r .image_name $config)
+config_version=$(jq -r ".dw_version //empty" $config)
 
 if [ -n "$config_version" ]; then
     if [ $config_version != $DW_VERSION ]; then
@@ -39,11 +39,20 @@ mkdir images/$imname
 
 cp -r resources/* images/$imname/
 cp resources/.dockerignore images/$imname/
+cp ../`jq -r .class $config` images/$imname/model_class.py
+jq -r '.additional | .[]' $config | xargs -I {} cp -r ../{} images/$imname/
+jq '{"model_name": .model_name, "description": .description, "rest_args": .rest_args}' $config > images/$imname/config.json
+cp $config images/$imname/.docker-wrapper-config.json
 
-cp ../`cat $config | jq -r .class` images/$imname/model_class.py
-cp -r ../`cat $config | jq -r .model` images/$imname/model
+# --
+# Symlink model
 
-if [[ $(jq -r ".bootstrap //empty" $config) ]]
+jq -r .model_path $config > images/$imname/model_path
+
+# --
+# Bootstrap image
+bootstrap_flag=$(jq -r ".bootstrap //empty" $config)
+if [[ $bootstrap_flag ]]
 then
     cp -r ../`jq -r .bootstrap $config` images/$imname/app-bootstrap    
 else
@@ -51,14 +60,9 @@ else
     echo "#!/bin/bash" > images/$imname/app-bootstrap/bootstrap.sh
 fi
 
-jq -r '.additional | .[]' $config | xargs -I {} cp -r ../{} images/$imname/
-
-cat $config | jq '{"model_name": .model_name, "description": .description, "rest_args": .rest_args}' > images/$imname/config.json
-cp $config images/$imname/.docker-wrapper-config.json
-
 # --
 # Use different base image?
-base_image=$(cat $config | jq -r ".base_image //empty")
+base_image=$(jq -r ".base_image //empty" $config)
 if [[ $base_image ]]
 then
     sed  -i 's@FROM .*@FROM '"$base_image"'@' images/$imname/Dockerfile
@@ -67,7 +71,7 @@ fi
 
 # --
 # Use NVIDIA-docker?
-gpu_flag=$(cat $config | jq -r ".gpu_flag //empty")
+gpu_flag=$(jq -r ".gpu_flag //empty" $config)
 if [[ $gpu_flag ]]
 then
     sed  -i 's/docker run/NV_GPU=1 nvidia-docker run/' images/$imname/quickstart.sh
@@ -75,7 +79,7 @@ fi
 
 # --
 # Use non-`gunicorn` server?
-legacy_flag=$(cat $config | jq -r ".legacy_flag //empty")
+legacy_flag=$(jq -r ".legacy_flag //empty" $config)
 if [[ $legacy_flag ]]
 then
     mv images/$imname/legacy-app.py images/$imname/generic-app.py
--- resources/quickstart.sh
+++ resources/quickstart.sh
@@ -2,4 +2,4 @@
 
 docker kill $(docker ps -q) 
 docker build -t docker_analytic .
-docker run -it -p 5000:5000 -v $(pwd):/scratch/ docker_analytic
+docker run -it -p 5000:5000 -v $(cat model_path):/scratch/model/ docker_analytic
